4fab70bfa9adfb5a1b5045930744626a6607074e
Aadishesh Padasalgi <aadishesh05@gmail.com>
2025-05-24 21:25:18 +0530

    Update run_commits.bat

--- run_commits.bat
+++ run_commits.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
 python auto_committer.py
-:: pause - (add this if you wanna check and verfiy commits yourself)
+:: "pause" - (add this if you wanna check and verfiy commits yourself)
